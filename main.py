79a0ab41c2fc87787f7ce9b664f7d8ce6dabb0d7
Vasia Zarkadoula <65190697+VasilikiZarkadoula@users.noreply.github.com>
2022-12-04 20:58:19 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
+from source.brute_force import BruteForce
 from source.compact_forward import CompactForward
 from source.graph_class import Graph
 import time
 
 # https://snap.stanford.edu/data/ca-AstroPh.html
 # undirected
-graph_path = "graphs/CA-AstroPh.txt"
+# graph_path = "graphs/toy_example_3_triangles.txt"
 
 # directed
 # graph_path = "graphs/email-Eu-core.txt"
-
+graph_path = "graphs/toy_example_directed.txt"
 
 def main():
     graph = Graph(graph_path)
+    print(graph)
     with_doulion = False
+    is_directed = True
 
-    compact_forward = CompactForward(graph, with_doulion).compact_forward
+    #compact_forward = CompactForward(graph, with_doulion).compact_forward
+    brute_force = BruteForce(graph, with_doulion, is_directed).brute_force
 
-    for algorithm in [compact_forward]:
+    for algorithm in [brute_force]:
         print(algorithm.__name__)
         start = time.time()
         triangles = algorithm()
@@ -25,7 +29,5 @@ def main():
         print(f'\t# triangles = {triangles}')
         print(f'\telapsed time = {end - start}')
 
-
 if __name__ == '__main__':
     main()
-
