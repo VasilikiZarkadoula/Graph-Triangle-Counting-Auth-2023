ae8428350c84fe5f70212910336c2682f1ba28a7
Vasia Zarkadoula <65190697+VasilikiZarkadoula@users.noreply.github.com>
2022-12-11 18:18:01 +0200

    Add files via upload

--- source/graph_class.py
+++ source/graph_class.py
@@ -2,10 +2,11 @@ import re
 
 class Graph:
     def __init__(self, graph_path):
-        self.graph = self.read_edge_list(graph_path)
+        self.graph, self.graph_edges = self.read_edge_list(graph_path)
 
     def read_edge_list(self, path):
         graph = {}
+        graph_edges = []
         with open(path, "r") as edges:
             for edge in edges:
                 if edge.startswith('#'):  # comment line
@@ -13,13 +14,14 @@ class Graph:
 
                 edge = re.split('\s+', edge)
                 v, u = (int(edge[i]) for i in range(2))  # v -> u
+                graph_edges.append((v, u))
                 try:
                     graph[v].append(u)
                 except KeyError:
                     graph[v] = [u]    # u is v's first neighbor
                 if u not in graph:
                     graph[u] = []
-        return graph
+        return graph, graph_edges
 
     def nodes(self):
         return [*self.graph]
