2a7c27e1ffd47832628fc26d6896d70a70812556
Vasia Zarkadoula <65190697+VasilikiZarkadoula@users.noreply.github.com>
2023-01-21 19:21:10 +0200

    Add files via upload

--- source/node_iterator.py
+++ source/node_iterator.py
@@ -5,28 +5,21 @@ class NodeIterator:
 
     def __init__(self, graph, graph_edges):
         self.graph = graph
-        self.graph_edges = graph_edges
-
-    def sort_graph(self):
-        self.graph = {key: sorted(self.graph[key]) for key in sorted(self.graph)}
+        self.graph_edges = set(graph_edges)
 
     def node_iterator(self):
-
-        self.sort_graph()
-        nodes = self.graph.nodes()
-
         count_triangles = 0
-        for node in nodes:
+        for node in self.graph.nodes():
             nbrs = []
             # find all pairs of neighbors {u,w} of node
             for u in self.graph.neighbors(node):
                 nbrs.append(u)
             # create neighbor pairs of 2
             nbrs_pair = list(combinations(nbrs, 2))
-            for i in range(0, len(nbrs_pair)):
-                if nbrs_pair[i] in self.graph_edges:
-                    nbrs_pair[i] = tuple(nbrs_pair[i])
+            for u, v in nbrs_pair:
+                # check if the pair is also an edge in the graph
+                if (u, v) in self.graph_edges or (v, u) in self.graph_edges:
                     # to avoid double counting, we count a triangle only if u < n < w
-                    if nbrs_pair[i][0] < node < nbrs_pair[i][1]:
-                        count_triangles = count_triangles + 1
+                    if u < node < v:
+                        count_triangles += 1
         return count_triangles
