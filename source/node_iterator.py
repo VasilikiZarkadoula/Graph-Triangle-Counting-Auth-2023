d5139959978103d255a73359947b3ac5a6c155f1
Vasia Zarkadoula <65190697+VasilikiZarkadoula@users.noreply.github.com>
2023-01-22 11:14:42 +0200

    Update node_iterator.py

--- source/node_iterator.py
+++ source/node_iterator.py
@@ -10,12 +10,7 @@ class NodeIterator:
     def node_iterator(self):
         count_triangles = 0
         for node in self.graph.nodes():
-            nbrs = []
-            # find all pairs of neighbors {u,w} of node
-            for u in self.graph.neighbors(node):
-                nbrs.append(u)
-            # create neighbor pairs of 2
-            nbrs_pair = list(combinations(nbrs, 2))
+            nbrs_pair = list(combinations(self.graph.neighbors(node), 2))
             for u, v in nbrs_pair:
                 # check if the pair is also an edge in the graph
                 if (u, v) in self.graph_edges or (v, u) in self.graph_edges:
